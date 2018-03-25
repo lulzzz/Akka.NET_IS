14fc178590cb10bab8d95cbfdce99d2c48b6e8d4
Wladimir <vladimirturovets@gmail.com>
2018-03-25 22:45:18 +0300

    Updates

---
+++ Akka.NET/Test/Send.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.MessagePatterns;
+using RabbitMQ.Client.Events;
+
+namespace Test
+{
+    class Send
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
