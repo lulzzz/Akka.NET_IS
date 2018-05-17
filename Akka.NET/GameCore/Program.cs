b0062c865a52442452f3c768d7e9fb7647cf3105
Wladimir <vladimirturovets@gmail.com>
2018-05-17 19:55:25 +0300

    FSM

---
+++ Akka.NET/GameCore/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Akka.Actor;
+using Akka.Event;
+using System.Collections.Immutable;
+
+namespace GameCore
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("TurnstileSimulator");
+            var turnstileActor = system.ActorOf<TurnstileActor>("Turnstile");
+
+            // пытаемся пройти - монет нет
+            turnstileActor.Tell(new PushBar());
+            // вставляем монету
+            turnstileActor.Tell(new InsertCoin());
+            // проходим
+            turnstileActor.Tell(new PushBar());
+            // пытается пройти заяц
+            turnstileActor.Tell(new PushBar());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Akka.NET/GameCore/TurnstileActor.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Akka.Actor;
+
+namespace GameCore
+{
+    #region States
+    public enum TurnstileStatus
+    {
+        Locked,
+        Unlocked
+    }
+    #endregion
+
+    #region Events
+
+    /// <summary>
+    /// событие - пытаемся пройти
+    /// </summary>
+    public class PushBar { }
+
+    /// <summary>
+    /// событие - вставляем монету
+    /// </summary>
+    public class InsertCoin { }
+
+    #endregion
+
+    // монеты
+    public class Coins
+    {
+        public int Total { get; }
+
+        public Coins(int total)
+        {
+            Total = total;
+        }
+
+        public Coins AddOne()
+        {
+            return new Coins(Total + 1);
+        }
+
+        public static Coins NoCoins()
+        {
+            return new Coins(0);
+        }
+    }
+
+    class TurnstileActor : FSM<TurnstileStatus, Coins>
+    {
+        public TurnstileActor()
+        {
+            InitializeFsm();
+        }
+
+        /// <summary>
+        /// инициализация
+        /// </summary>
+        private void InitializeFsm()
+        {
+            StartWith(TurnstileStatus.Locked, Coins.NoCoins());
+            When(TurnstileStatus.Locked, LockedLogic);
+            When(TurnstileStatus.Unlocked, UnlockedLogic);
+            Initialize();
+        }
+
+        /// <summary>
+        /// логика при разблокированном состоянии
+        /// </summary>
+        /// <param name="fsmevent"></param>
+        /// <returns></returns>
+        private State<TurnstileStatus, Coins> UnlockedLogic(Event<Coins> fsmevent)
+        {
+            // слишком много монет, получаем сдачу, состояние не меняется
+            if (fsmevent.FsmEvent is InsertCoin)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Coin already inserted. Take back and go.");
+                Console.ResetColor();
+                return GoTo(TurnstileStatus.Unlocked).Using(StateData.AddOne());
+            }
+            // проходим
+            if (fsmevent.FsmEvent is PushBar)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"You can go. State is: {StateName}.");
+                Console.ResetColor();
+                return GoTo(TurnstileStatus.Locked);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// логика при блокированном состоянии
+        /// </summary>
+        /// <param name="fsmevent"></param>
+        /// <returns></returns>
+        private State<TurnstileStatus, Coins> LockedLogic(Event<Coins> fsmevent)
+        {
+            // вставляем монету
+            if (fsmevent.FsmEvent is InsertCoin)
+            {
+                return GoTo(TurnstileStatus.Unlocked).Using(StateData.AddOne());
+            }
+            // пытаемся пройти без монеты
+            if (fsmevent.FsmEvent is PushBar)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"You can't go, insert coin first please. State is {StateName}.");
+                Console.ResetColor();
+                return Stay();
+            }
+            return null;
+        }
+    }
+}
+
